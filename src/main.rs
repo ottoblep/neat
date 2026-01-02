7b950ac733f9f8c9ab9a2f3e26d75bec3e83e4a7
Severin Lochschmidt <seviron53@gmail.com>
2026-01-02 21:42:38 +0100

    Remove argparser for now

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ mod genome;
 mod individual;
 mod population;
 
-use clap::Parser;
 use data::TestSet;
 use nalgebra::dvector;
 
@@ -11,29 +10,7 @@ const NUM_GENERATIONS: usize = 100;
 const N_POP: usize = 100;
 const N_FITTEST_REPRODUCE: usize = 20;
 
-#[derive(Parser, Debug)]
-#[clap(author = "", version, about)]
-/// Application configuration
-struct Args {
-    /// whether to be verbose
-    #[arg(short = 'v')]
-    verbose: bool,
-
-    /// an optional name to greet
-    #[arg()]
-    name: Option<String>,
-}
-
 fn main() {
-    let args = Args::parse();
-    if args.verbose {
-        println!("DEBUG {args:?}");
-    }
-    println!(
-        "Hello {} (from neat)!",
-        args.name.unwrap_or("world".to_string())
-    );
-
     let xor_test_inputs: TestSet = TestSet {
         inputs: vec![
             dvector![0.0, 0.0],
