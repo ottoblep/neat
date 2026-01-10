f5dfe7865c745d6425100127320b52301b86ce90
Severin Lochschmidt <seviron53@gmail.com>
2026-01-10 17:15:56 +0100

    Spin off chart creation

--- src/tui.rs
+++ src/tui.rs
@@ -5,27 +5,41 @@ use ratatui::{
     style::{Color, Modifier, Style},
     symbols,
     text::Span,
-    widgets::{Axis, Block, Chart, Dataset},
+    widgets::{Axis, Block, Chart, Dataset, GraphType},
 };
 
 use crate::statistics::PopulationStatSeries;
 
-pub fn draw<'a>(
-    terminal: &'a mut ratatui::DefaultTerminal,
-    stats: &'a PopulationStatSeries,
-) -> Result<CompletedFrame<'a>, Error> {
-    let avg_fitness = stats.get_average_fitness_series();
+type Limits = (f64, f64);
+
+fn chart<'a>(
+    title: &'a str,
+    x_label: &'a str,
+    y_label: &'a str,
+    points: &'a Vec<(f64, f64)>,
+) -> Chart<'a> {
+    let x_limits: Limits = (0.0, (points.len().saturating_sub(1) as f64));
+    let mut y_limits: Limits = points.iter().fold((f64::MIN, f64::MAX), |acc, (_, a)| {
+        (acc.0.min(*a), acc.1.max(*a))
+    });
+
+    let pad = ((y_limits.0 - y_limits.0) * 0.1).max(0.5);
+    y_limits.0 -= pad;
+    y_limits.1 += pad;
+
     let datasets = vec![
         Dataset::default()
-            .name("Average Fitness")
+            .name(title)
             .marker(symbols::Marker::Dot)
+            .graph_type(GraphType::Scatter)
             .style(Style::default().fg(Color::Cyan))
-            .data(&avg_fitness),
+            .data(&points),
     ];
-    let chart = Chart::new(datasets)
+
+    Chart::new(datasets)
         .block(
             Block::bordered().title(Span::styled(
-                "Fitness",
+                title,
                 Style::default()
                     .fg(Color::Cyan)
                     .add_modifier(Modifier::BOLD),
@@ -33,20 +47,33 @@ pub fn draw<'a>(
         )
         .x_axis(
             Axis::default()
-                .title("Generation")
-                .style(Style::default().fg(Color::Gray)),
+                .title(x_label)
+                .style(Style::default().fg(Color::Gray))
+                .bounds([x_limits.0, x_limits.1])
+                .labels([
+                    Span::raw(format!("{:.2}", x_limits.0)),
+                    Span::raw(format!("{:.2}", (x_limits.0 + x_limits.1) / 2.0)),
+                    Span::raw(format!("{:.2}", x_limits.1)),
+                ]),
         )
         .y_axis(
             Axis::default()
-                .title("Fitness")
+                .title(y_label)
                 .style(Style::default().fg(Color::Gray))
-                .bounds([-20.0, 20.0])
+                .bounds([y_limits.0, y_limits.1])
                 .labels([
-                    Span::styled("-20", Style::default().add_modifier(Modifier::BOLD)),
-                    Span::raw("0"),
-                    Span::styled("20", Style::default().add_modifier(Modifier::BOLD)),
+                    Span::raw(format!("{:.2}", y_limits.0)),
+                    Span::raw(format!("{:.2}", (y_limits.0 + y_limits.1) / 2.0)),
+                    Span::raw(format!("{:.2}", y_limits.1)),
                 ]),
-        );
+        )
+}
 
+pub fn draw<'a>(
+    terminal: &'a mut ratatui::DefaultTerminal,
+    stats: &'a PopulationStatSeries,
+) -> Result<CompletedFrame<'a>, Error> {
+    let avg_fitness = stats.get_average_fitness_series();
+    let chart = chart("Average Fitness", "Generation", "Fitness", &avg_fitness);
     terminal.draw(|frame| frame.render_widget(chart, frame.area()))
 }
